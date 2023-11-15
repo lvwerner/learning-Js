9810445b540b04d90b10a48343d635538c6afc11
Vini Werner <118356029+lvwerner@users.noreply.github.com>
2023-11-15 19:27:01 -0300

    Add files via upload

---
+++ JavaScript/1aula.js
@@ -0,0 +1,2 @@
+var nome = window.prompt('Qual é o seu nome?') //vai perguntar o seu nome 
+alert('Olá, ' + nome + '! Prazer em te conhecer!') //vai aparecer uma mensagem de boas vindas com o seu nome
---
+++ JavaScript/aula 6/aula2.js
@@ -0,0 +1,4 @@
+var n1 = window.prompt("Digite um número: ");
+var n2 = window.prompt("Digite outro número: ");
+var s = Number(n1) + Number(n2);
+alert("A soma entre " + n1 + " e " + n2 + " é igual a " + s + ".");
